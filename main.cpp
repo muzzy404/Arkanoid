6d3b1b8003a5585a5c24b2041b8f6045b33baaab
Daria <da_green@mail.ru>
2016-05-05 00:25:26 +0300

    some basic classes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,40 @@
 #define screenWidth 800
 #define screenHight 600
 
+class GameData{
+	char *lvlMap;
+	int score;
+	int life;
+public:
+	GameData();
+	void newLvl();
+	void addScore(int add) { score += add; };
+	int getScore() { return score; };
+	int getLife() { return life; };
+	void loseLife() { if (life > 0) life--; };
+};
+
+class Visual : public sf::Drawable{
+	int x, y;
+	int width, height;
+public:
+	Visual() { x = 0; y = 0; };
+	Visual(int x0, int y0) { x = x0; y = y0; };
+	int getX() { return x; };
+	int getY() { return y; };
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
+};
+
+class Dinamic : public Visual{
+	int speed;                  //the numb of cycle till the next redraw method
+	int tillRedraw;
+	virtual void moveObj() = 0; //change coordinates of object
+public:
+	Dinamic(int startSpeed) { speed = startSpeed; };
+	void drawCycle() { tillRedraw--; };
+	int getTillRedraw() { return tillRedraw; };
+};
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(screenWidth, screenHight), "Arkanoid");
