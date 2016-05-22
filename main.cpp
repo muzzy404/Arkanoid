f8fab53dbe751135b1b12945b6c77035f04d04af
Daria <da_green@mail.ru>
2016-05-22 18:29:28 +0300

    now we have a platform!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 #define screenWidth 1000
 #define screenHight 600
-#define ballRadius 8.0
+#define ballRadius  8.0
+#define platformWidth (6 * ballRadius)  
+#define platformHeight  (2 * ballRadius) 
 #define gameFieldWidth (0.60 * screenWidth)
 #define gameFieldHight (0.80 * screenHight)
 #define fieldBorderX   (0.05 * screenWidth)
@@ -23,11 +25,23 @@ public:
 	void loseLife() { if (life > 0) life--; };
 };
 
-class Ball{
-	sf::CircleShape shape;
+class DinamicVisual{
+protected:
 	sf::Vector2f speed;
 	float d_speed;
 public:
+	virtual void update()  = 0;
+	virtual float x() = 0;
+	virtual float y() = 0;
+	virtual float left() = 0;
+	virtual float right() = 0;
+	virtual float top() = 0;
+	virtual float bottom() = 0;
+};
+
+class Ball : public DinamicVisual{
+public:
+	sf::CircleShape shape;
 	Ball(float startX, float startY, float ballSpeed)
     {
 		d_speed = ballSpeed;
@@ -39,7 +53,6 @@ public:
         shape.setOrigin(ballRadius, ballRadius);
 	};
 	void set_d_speed(float newSpeed){ d_speed = newSpeed; }
-	sf::CircleShape getBall(){ return shape; }
 	//получить параметры мячика
 	float x()      { return shape.getPosition().x; }
 	float y()      { return shape.getPosition().y; }
@@ -50,9 +63,32 @@ public:
 	void update();
 };
 
-class BackGround{
-	sf::RectangleShape shape;
+class Platform : public DinamicVisual{
 public:
+	sf::RectangleShape shape;
+	Platform(float startX, float startY, float platformSpeed)
+	{
+		d_speed = platformSpeed;
+		speed.x = 0.0;
+		speed.y = 0.0;
+		shape.setPosition(startX, startY - platformHeight);
+        shape.setSize({platformWidth, platformHeight});
+        shape.setFillColor(sf::Color::Red);
+        shape.setOrigin((platformWidth / 2.0), (platformHeight / 2.0));
+	}
+	//получить параметры платформы
+	float x()      { return shape.getPosition().x; }
+    float y()      { return shape.getPosition().y; }
+    float left()   { return x() - shape.getSize().x / 2.0; }
+    float right()  { return x() + shape.getSize().x / 2.0; }
+    float top()    { return y() - shape.getSize().y / 2.0; }
+    float bottom() { return y() + shape.getSize().y / 2.0; }
+	void update();
+};
+
+class BackGround{
+public:
+	sf::RectangleShape shape;
 	BackGround(float startX, float startY)
 	{
 		shape.setPosition(startX, startY);
@@ -60,13 +96,12 @@ public:
 		shape.setSize(gameFieldSize);
 		shape.setFillColor(sf::Color::Black);
 	};
-	sf::RectangleShape getBackGround(){ return shape; }
 };
 
 void Ball::update()
 {
 	shape.move(speed);
-	//Мячик должен находиться внутри экрана
+	//Мячик должен находиться внутри игрового поля
 	//Проверки местоположения и последующие настойки скорости
     //горизонтальная скорость
 	if (left() < fieldBorderX) speed.x = d_speed;
@@ -76,25 +111,54 @@ void Ball::update()
 	else if (bottom() > (fieldBorderY + gameFieldHight)) speed.y = -d_speed;
 }
 
+void Platform::update()
+{
+	shape.move(speed);
+	//Платформа должена находиться внутри игрового поля
+	//Проверка нажатия стрелок клавиатуры для перемещения платфомы
+	//Если есть нажание, необходимо изменить скорость
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && left() > fieldBorderX + d_speed)
+		speed.x = -d_speed;
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && right() < (fieldBorderX + gameFieldWidth) - d_speed)
+			speed.x = d_speed;
+		//если нажатия нет или платфома достигла края игрового поля, остановить движение
+        else
+            speed.x = 0.0;
+    }
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(screenWidth, screenHight), "Arkanoid");
 	window.setFramerateLimit(60);
 	
 	//объекты
-	Ball ball{(screenWidth / 2), (screenHight / 2), 6.0};
+	Ball ball{(screenWidth / 2.0), (screenHight / 2.0), 6.0};
 	BackGround bkGround{fieldBorderX, fieldBorderY};
-	
-	while(true)
+	Platform platform{ (gameFieldWidth / 2.0 + fieldBorderX), (fieldBorderY + gameFieldHight), 5.0};
+
+	while(window.isOpen())
     {
-        window.clear(sf::Color::Cyan);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) break;
-
-        ball.update();
-
-		window.draw(bkGround.getBackGround());
-        window.draw(ball.getBall());
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+		}
+		
+		ball.update();
+		platform.update();
+		
+		window.clear(sf::Color::White);
+		window.draw(bkGround.shape);
+        window.draw(ball.shape);
+		window.draw(platform.shape);
         window.display();
     }
 	return 0;
