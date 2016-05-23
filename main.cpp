30c48046207197775379ac34bbf847344dd0b350
Daria <da_green@mail.ru>
2016-05-24 00:52:15 +0300

    bricks (with textures) without collisions

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 #define screenWidth 800
 #define screenHight 600
 #define ballRadius  6.0
-#define platformWidth  (10.0 * ballRadius)  
+#define platformWidth  (12.0 * ballRadius)  
 #define platformHeight (2.0 * ballRadius) 
 #define gameFieldWidth (0.50 * screenWidth)
 #define gameFieldHight (0.90 * screenHight)
 #define fieldBorderX   (0.05 * screenWidth)
 #define fieldBorderY   (0.05 * screenHight)
+#define brickWidth 80
+#define brickHight 20
 
 class GameData{
 	char *lvlMap;
@@ -24,21 +26,68 @@ public:
 	void loseLife() { if (life > 0) life--; };
 };
 
-class DinamicVisual{
+class Visual{
+public:
+	virtual float x()      = 0;
+	virtual float y()      = 0;
+	virtual float left()   = 0;
+	virtual float right()  = 0;
+	virtual float top()    = 0;
+	virtual float bottom() = 0;
+};
+
+class Brick : public Visual{
+	int durability;
+	bool notBroken;
+	sf::Texture normalTexture;
+	sf::Texture brokenTexture;
+	sf::FloatRect spriteRect;
+public:
+	sf::Sprite sprite;
+	Brick(float startX, float startY)
+	{
+		int randSprite = std::rand() % 3 + 1; //случайное число от 1 до 3
+		//загрузка необходимой текстуры - разные кирпичики
+		switch (randSprite)
+		{
+		case 1:
+			if (!normalTexture.loadFromFile("br_blue.png"))
+				std::cout << "can't find a brick image 1\n";
+			break;
+		case 2:
+			if (!normalTexture.loadFromFile("br_red.png"))
+				std::cout << "can't find a brick image 2\n";
+			break;
+		case 3:
+			if (!normalTexture.loadFromFile("br_green.png"))
+				std::cout << "can't find a brick image 3\n";
+			break;
+		}
+		sprite.setTexture(normalTexture);
+		sprite.setPosition(startX, startY);
+		spriteRect = sprite.getGlobalBounds();
+		sprite.setOrigin(spriteRect.width / 2.0, spriteRect.height / 2.0);
+		durability = 1;//временно надежность у всех одинаковая
+		notBroken = true;
+	}
+	float x()      { return sprite.getPosition().x; }
+	float y()      { return sprite.getPosition().y; }
+	float left()   { return x() - spriteRect.width / 2.0;  }
+    float right()  { return x() + spriteRect.width / 2.0;  }
+    float top()    { return y() - spriteRect.height / 2.0; }
+    float bottom() { return y() + spriteRect.height / 2.0; }
+	
+};
+
+class DinamicVisual : public Visual{
 protected:
 	sf::Vector2f speed;
 	float d_speed;
 public:
+	virtual void update()  = 0;
 	void setSpeedX(float newSpeed){ speed.x = newSpeed; }
 	void setSpeedY(float newSpeed){ speed.y = newSpeed; }
 	float get_d_speed(){ return d_speed; }
-	virtual void update()  = 0;
-	virtual float x() = 0;
-	virtual float y() = 0;
-	virtual float left() = 0;
-	virtual float right() = 0;
-	virtual float top() = 0;
-	virtual float bottom() = 0;
 };
 
 template <class firstClass, class secondClass>
@@ -144,7 +193,7 @@ void Ball::update()
 void Platform::update()
 {
 	shape.move(speed);
-	//Платформа должена находиться внутри игрового поля
+	//Платформа должна находиться внутри игрового поля
 	//Проверка нажатия стрелок клавиатуры для перемещения платфомы
 	//Если есть нажание, необходимо изменить скорость
 	if( (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) && (left() > fieldBorderX + d_speed))
@@ -156,6 +205,7 @@ void Platform::update()
             speed.x = 0.0;
 }
 
+//класс, проверяющий столкновения объектов
 class Collisions{
 public:
 	void ballAndPlatform(Platform& platformObj, Ball& ballObj)
@@ -178,10 +228,22 @@ int main()
 	window.setFramerateLimit(60);
 	
 	//объекты
-	Ball ball{5.0};
+	Ball ball{7.0};
 	BackGround bkGround{fieldBorderX, fieldBorderY};
 	Platform platform{ (gameFieldWidth / 2.0 + fieldBorderX), (fieldBorderY + gameFieldHight), 8.0};
 	Collisions collisions;
+	
+	//временное размещение кирпичей рядами без карты уровня
+	const int maxBricksX = 4;
+	std::vector<Brick> bricks;
+	for (int i = 0; i < maxBricksX; i++)
+		for (int j = 0; j < 9; j++)
+		{
+			float brickX = (i + 1) * (brickWidth + 3) + fieldBorderX + ((gameFieldWidth - (brickWidth + 3) * maxBricksX) / 2.0) - brickWidth / 2.0;
+			float brickY = (j + 1) * (brickHight + 3) + fieldBorderY - brickHight / 2.0;
+			Brick* newBrick = new Brick(brickX, brickY);
+			bricks.push_back(*newBrick);
+		}
 
 	sf::Event event;
 
@@ -211,13 +273,17 @@ int main()
 			collisions.ballAndPlatform(platform, ball);
 		}
 		
-		window.clear(sf::Color::White);
+		window.clear(sf::Color::Black);
 		window.draw(bkGround.sprite);
-        window.draw(ball.shape);
 		window.draw(platform.shape);
+		//прорисовка вектора кирпичей
+		for (int i = 0; i < bricks.size(); i++)
+			window.draw(bricks[i].sprite);
+		window.draw(ball.shape);
+
         window.display();
 
-		while (!gameStart){
+ 		while (!gameStart){
 			window.pollEvent(event);
 			if (event.key.code == sf::Keyboard::Space) gameStart = true;
 		}
